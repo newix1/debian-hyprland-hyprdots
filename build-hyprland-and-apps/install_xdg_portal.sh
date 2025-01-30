0a6bfd13c97105adfa53914238da0c722a82fc49
newix1 <123808307+newix1@users.noreply.github.com>
2025-01-30 10:13:39 +0300

    Update install_xdg_portal.sh

--- build-hyprland-and-apps/install_xdg_portal.sh
+++ build-hyprland-and-apps/install_xdg_portal.sh
@@ -2,12 +2,8 @@
 
 
 # Clone and build xdg-desktop-portal-hyprland
-git clone https://github.com/hyprwm/xdg-desktop-portal-hyprland.git
-cd xdg-desktop-portal-hyprland
-cd subprojects
-git clone https://github.com/hyprwm/hyprland-protocols.git
-git clone https://github.com/Kistler-Group/sdbus-cpp.git
-cd ..
-make all
-sudo make install
-cd ..
+git clone --recursive https://github.com/hyprwm/xdg-desktop-portal-hyprland
+cd xdg-desktop-portal-hyprland/
+cmake -DCMAKE_INSTALL_LIBEXECDIR=/usr/lib -DCMAKE_INSTALL_PREFIX=/usr -B build
+cmake --build build
+sudo cmake --install build
